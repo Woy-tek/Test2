a3faa6b2c096ae5890779e8839c7276a1289d654
Woy-tek <woy1157@gmail.com>
2017-03-09 19:09:52 +0100

    Initial commit

---
+++ src/Klasa.java
@@ -0,0 +1,9 @@
+/**
+ * Created by woy on 09.03.17.
+ */
+public class Klasa {
+    public static void main(String[] args){
+        System.out.println("AAA");
+       // System.out.println();
+    }
+}
