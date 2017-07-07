1a4397b9385f296d50d371481baa3e2af19fcb07
Wojciech Tryba <wojciech.tryba@stazysta.comarch.pl>
2017-07-07 08:11:47 +0200

    Commit

--- src/Klasa.java
+++ src/Klasa.java
@@ -5,6 +5,7 @@ public class Klasa {
     public static void main(String[] args){
         System.out.println("AAA");
         System.out.println("BBB");
+        System.out.println("CCC");
 //aaa
     }
 }
